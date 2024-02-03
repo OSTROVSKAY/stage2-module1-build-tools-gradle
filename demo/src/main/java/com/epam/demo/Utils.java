5b29c1a490a5ef4b1a44cf1af807fc20b63236c4
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2024-02-03 10:04:51 +0300

    Finish : Module #1. Task #1. Build Tools. Gradle

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,5 +5,34 @@ import java.util.List;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         //magic happens here
+
+        boolean x = false;
+
+        if(args.contains(null) || args == null) {
+
+            x = false;
+
+        } else {
+
+            int count = 0;
+
+            for(String str: args) {
+
+                if(str.startsWith("-") || str.equals("0") || str.startsWith(" ") || str.equals("")) {
+
+                    count++;
+                }
+
+            }
+
+            if(count == 0) {
+
+                x = true;
+            }
+
+        }
+
+        return x;
+
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,21 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+
+        boolean x = false;
+
+        if(str == null) {
+
+            x = false;
+
+        } else if (!str.startsWith("-") || !str.equals("0") || !str.startsWith(" ") || !str.equals(""))  {
+
+            x = true;
+
+        }
+
+        return x;
+
          //here magic will happen
     }
 }
