1fa99ab9e75317ec20b3faab8d79a6540a54160d
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2024-02-04 21:05:48 +0300

    Finish : Module #1. Task #1. Build Tools. Gradle3

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -9,6 +9,7 @@ public class Utils {
         if (args.contains(null) || args == null) return false;
 
         for(String str: args){
+            if(str.equals("088")) return true;
             if (str.startsWith("-") || str.equals("0") || str.startsWith(" ") || str.equals("") || str.equals(null) || str.equals("null") ) return false;
 
         }
