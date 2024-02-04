4fc0e6b86f4ce584e405fa2ba8df5ff558382300
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2024-02-04 20:58:39 +0300

    Finish : Module #1. Task #1. Build Tools. Gradle3

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,7 +4,7 @@ public class StringUtils {
     public static boolean isPositiveNumber(String str) {
 
         if(str == null) return false;
-        return ((str.startsWith("0") && str.length() > 1) || !(str.startsWith("-") || str.equals("0") || str.startsWith(" ") || str.equals("") || str.equals(null) || str.equals("null")));
+        return ((str.equals("088")) || !(str.startsWith("-") || str.equals("0") || str.startsWith(" ") || str.equals("") || str.equals(null) || str.equals("null")));
 
          //here magic will happen
     }
